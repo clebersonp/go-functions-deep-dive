e86382bf9c85ee5c01a07479851e9a4c43880343
Cleberson <pauluci.cleberson@gmail.com>
2024-02-13 22:46:58 -0300

    Created a function passing a function as value and created two function as custom types

--- main.go
+++ main.go
@@ -1,5 +1,39 @@
 package main
 
+import "fmt"
+
+// store function type as custom type
+type transformFn func(int) int
+
+// just for example of a more complex func signature as a custom type
+type anotherFn func(int, []string, map[string][]int)
+
 func main() {
 
+	numbers := []int{1, 2, 3}
+	fmt.Println(numbers)
+
+	// function as first class values, passing function around as values just as you can pass other kind of values around
+	// functions are just values in Go at the end
+	doubled := transformNumbers(&numbers, double)
+	fmt.Println(doubled)
+
+	tripled := transformNumbers(&numbers, triple)
+	fmt.Println(tripled)
+}
+
+func transformNumbers(numbers *[]int, transform transformFn) []int {
+	var transformResult []int // nil slice declaration
+	for _, number := range *numbers {
+		transformResult = append(transformResult, transform(number))
+	}
+	return transformResult
+}
+
+func double(number int) int {
+	return number * 2
+}
+
+func triple(number int) int {
+	return number * 3
 }
