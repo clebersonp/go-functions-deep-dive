190a1a8cedf147349c00ac32812b94115645421e
Cleberson <pauluci.cleberson@gmail.com>
2024-02-14 08:47:02 -0300

    Refactored an moved recursion example to own package to start variadic study

--- main.go
+++ main.go
@@ -1,32 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
-	number := 6
-	fac := factorial(number)
-	fmt.Printf("%v!: %v\n", number, fac)
 
-	fmt.Println("-----------------Recursion---------------")
-	fac = factorialRecursion(number)
-	fmt.Printf("%v!: %v\n", number, fac)
-}
-
-// factorial => 5! => 5 * 4 * 3 * 2 * 1 = 120
-func factorial(number int) int {
-	result := 1
-
-	for i := 1; i <= number; i++ {
-		result *= i
-	}
-
-	return result
-}
-
-func factorialRecursion(number int) int {
-	// defined exit condition, otherwise will be in infinity loop
-	if number <= 1 {
-		return 1
-	}
-	return number * factorialRecursion(number-1)
 }
